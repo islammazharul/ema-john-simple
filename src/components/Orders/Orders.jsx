1b99ceac1eb07c3aa6ec23c9897044ac5cb1aa08
islammazharul <islammazharul3645@gmail.com>
2023-04-05 02:10:45 +0600

    add cart item

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
+import ReviewItems from '../Review/ReviewItems';
+import './Order.css'
 
 const Orders = () => {
     const cart = useLoaderData();
-    // console.log(products)
+    console.log(cart)
     return (
         <div className='shop-container'>
-            <div className='product-container'>
-                <h2>Product Items: {cart.length}</h2>
+            <div className='review-container'>
+                {/* <h2>Product Items: {cart.length}</h2> */}
+                {
+                    cart.map(product => <ReviewItems key={product.id}
+                    product={product}
+                    ></ReviewItems>)
+                }
             </div>
             <div className='cart-container'>
                 <Cart cart={cart}></Cart>
